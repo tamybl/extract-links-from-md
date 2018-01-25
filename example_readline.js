b385f8298eca45e6933e42c32477d5f6a57b93c7
camilabelen <ccornejoss@gmail.com>
2018-01-25 09:07:38 -0300

    Cambio de expresión regular para url

--- example_readline.js
+++ example_readline.js
@@ -10,7 +10,7 @@ function inputText (text) {
   }
 
 /* EXAMPLE (Copy) 
-# Lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te mpor incididunt ut [labore](https://en.wiktionary.org/wiki/labore) et [dolore](https://en.wiktionary.org/wiki/dolore) magna al iqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. [foo](http://foo.com) Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sin t occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+# Lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te mpor incididunt ut [labore](en.wiktionary.org/wiki/labore) et [dolore](https://en.wiktionary.org/wiki/dolore) magna al iqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. [foo](www.foo.com) Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sin t occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 */
 
 
--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var getMdLinks = function(str) {
   let matchesArr = str.match(regex)
 
   var arrResult = matchesArr.map(function(element) {
-    var url = element.match(/\bhttps?:\/\/\S+/gi); // enlace REVISAR REGEX CORRECTA
-    //var text = element.match(/(?<=\[).+?(?=\])/gi); //text
+    var url = element.match(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi); // enlace REVISAR REGEX CORRECTA
+    //var text = element.match((\w+)\s+\1); //text
     var obj = { href: url.toString(), text: null }
     return obj
   })

b3da12bee66ae9c877066d76969799baeb491b37
tamybl <tamara.bravo.l@gmail.com>
2018-01-25 09:20:28 -0300

    Modificaciones expresion regular texto (text)

--- example_readline.js
+++ example_readline.js
@@ -9,7 +9,8 @@ function inputText (text) {
   console.log(geturls(text))
   }
 
-/* EXAMPLE (Copy) 
+
+/* EXAMPLE (Copy)
 # Lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te mpor incididunt ut [labore](en.wiktionary.org/wiki/labore) et [dolore](https://en.wiktionary.org/wiki/dolore) magna al iqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. [foo](www.foo.com) Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sin t occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 */
 
--- index.js
+++ index.js
@@ -6,8 +6,9 @@ var getMdLinks = function(str) {
 
   var arrResult = matchesArr.map(function(element) {
     var url = element.match(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi); // enlace REVISAR REGEX CORRECTA
-    //var text = element.match((\w+)\s+\1); //text
-    var obj = { href: url.toString(), text: null }
+    var textBrackets = element.match(/\[(.*?)\]/g); //Texto con Corchetes
+    var text = textBrackets.substring(1, textBrackets.length-1)
+    var obj = { href: url.toString(), text: text }
     return obj
   })
 

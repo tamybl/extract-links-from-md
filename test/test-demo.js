4cd1095d3e31b0fc5a28b4e8fc84f4a5412b930c
camilabelen <ccornejoss@gmail.com>
2018-01-26 09:01:19 -0300

    actualizacion en test

--- test/test-demo.js
+++ test/test-demo.js
@@ -1,11 +1,35 @@
 const chai = require('chai');
-const getUrls = require('../src/get-links.js')
+const getMdLinks = require('../src/get-links.js');
 
-describe('Validar Links', function() {
+describe('Validar', function() {
   it('Debe comprobar si existe patron []()', () => {
-    chai.assert.equal('')
-  })
+    chai.assert.equal(getMdLinks.MdLink(''), true);
+  });
+  it('Debe comprobar si existe patron []()', () => {
+    chai.assert.equal(getMdLinks.MdLink(''), false);
+  });  
+});
+
+describe('Validar', function () {
   it('Debe comprobar si es una url valida', () => {
-    chai.assert.equal('')
-  })
-})
+    chai.assert.typeOf(getMdLinks.url(''), string);
+  });
+  it('Debe comprobar si es una url valida', () => {
+    chai.assert.equal(getMdLinks.url(''), false);
+  });
+});
+
+describe('Validar', function () {
+  it('Debe comprobar si retorna el nombre de link', () => {
+    chai.assert.equal(getMdLinks.text(''), true);
+  });
+  it('Debe comprobar si retorna el nombre de link', () => {
+    chai.assert.equal(getMdLinks.text(''), false);
+  });
+});
+
+describe('Validar', function () {
+  it('Debe comprobar si ', () => {
+    chai.assert.equal('');
+  });
+});

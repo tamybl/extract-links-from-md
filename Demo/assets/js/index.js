1da6eb33833d7bc0b2cbc78f3a6da0d53c10635f
camilabelen <ccornejoss@gmail.com>
2018-02-01 16:29:52 -0300

    demo

---
+++ Demo/assets/js/index.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+ 
+};
+
+const readline = require('readline');
+const button = document.getElementById('send');
+const input = document.getElementById('mdText');
+const results = document.getElementById('results');
+
+button.addEventListener('click', function () {
+  let text = input.value;
+});
